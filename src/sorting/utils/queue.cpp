8397abf584209cdf8c8229d6138958a537dc0892
bxchaney <bxchaney@gmail.com>
2023-04-28 18:49:12 -0400

    finished Queue implementation

--- src/sorting/utils/queue.cpp
+++ src/sorting/utils/queue.cpp
@@ -1,24 +1,14 @@
 #include<memory>
 #include"queue.h"
-#include<map>
-
-template <typename T> template<typename U>
-Queue<T>::__node<U>::__node(U data)
-{
-    this-> data = data;
-    next = nullptr;
-}
-
 
 template<typename T>
-
 Queue<T>::Queue(Queue<T>& other)
 {
     _head = nullptr;
     _tail = nullptr;
     _size = 0;
 
-    std::shared_ptr<Queue<T>::Node> curr_node = other._head;
+    node_pointer curr_node = other._head;
     while(curr_node)
     {
         enqueue(curr_node->data);
@@ -26,29 +16,96 @@ Queue<T>::Queue(Queue<T>& other)
     }
 }
 
-template <typename T>
-void Queue<T>::enqueue(T data)
+template<typename T>
+Queue<T>::~Queue()
 {
-    _size++;
+    node_pointer current = _head;
+    node_pointer next;
+    while(current)
+    {
+        next = current->next;
+        current->~Node();
+        current = next;
+    }
+}
+
+template<typename T>
+Queue<T>& Queue<T>::operator=(const Queue<T>& other)
+{
+    _head = nullptr;
+    _tail = nullptr;
+    _size = 0;
+
+    node_pointer curr_node = other._head;
+    while(curr_node)
+    {
+        enqueue(curr_node->data);
+        curr_node = curr_node->next;
+    }
+    return *this;
+}
+
+template <typename T>
+void Queue<T>::enqueue(value_type data)
+{
     
-    std::shared_ptr<Node> 
-    new_node std::shared_ptr<Node>(new Node(data));
+    
+    node_pointer new_node = std::shared_ptr<node_type>(new Node(data));
         
     // Queue is empty
     if (_head == nullptr)
     {
-        _head = new_node
-        _tail = head;
+        _head = new_node;
+        _tail = _head;
     }
     else
     {
+        // add new nodes to the tail of the list
         _tail->next = new_node;
+        new_node->prev = _tail;
         _tail = new_node;
     }
+    _size++;
+}
+
+template<typename T>
+typename Queue<T>::value_type Queue<T>::dequeue()
+{
+    node_pointer node = _head;
+        
+    // one node remaining
+    if (_head == _tail)
+    {
+        _head = _tail = nullptr;
+    }
+    else
+    {
+        _head = _head->next;
+        _head->prev = nullptr;
+    }
+    node->next = nullptr;
+    _size--;
+    return node->data;
 }
 
 template <typename T>
-int Queue<T>::size()
+size_t Queue<T>::size()
 {
     return _size;
 }
+
+template<typename T>
+std::ostream& operator<< (std::ostream& os, Queue<T>& q)
+{
+    std::shared_ptr<Node<T>> current = q._head;
+    while(current)
+    {
+        os << current->data << std::endl;
+        current = current->next;
+    }
+    return os;
+}
+
+template class Node<int>;
+template class Queue<int>;
+template std::ostream& operator<< <int>(std::ostream&, Queue<int>&);
--- src/sorting/utils/queue.h
+++ src/sorting/utils/queue.h
@@ -1,34 +1,49 @@
 #pragma once
+#include<iostream>
 #include<memory>
+
+template<typename T>
+struct Node
+{
+    public:
+        typedef T value_type;
+        typedef T& reference;
+        typedef const T& const_reference;
+
+        value_type data;
+        std::shared_ptr<Node<T>> next, prev;
+        Node(reference data) : data(data), next(nullptr), prev(nullptr){}
+        ~Node() = default;
+};
+
+
 
 template<typename T>
 class Queue
 {
     private:
-        template<typename U>
-        class __node
-        {
-            public:
-            U data;
-            std::shared_ptr<__node> next;
-            __node() = default;
-            __node(U);
-        };
+        
+        typedef Node<T> node_type;
+        typedef std::shared_ptr<node_type> node_pointer;
 
-        typedef __node<T> Node;
-
-        std::shared_ptr<Node> _head;
-        std::shared_ptr<Node> _tail;
-        int _size;
+        node_pointer _head, _tail;
+        size_t _size;
 
     public:
+        typedef typename node_type::value_type value_type;
+        typedef typename node_type::reference reference;
+        typedef typename node_type::const_reference const_reference;
+
         Queue() = default;
         ~Queue();
 
         Queue(Queue<T>&);
-        Queue& operator=(Queue&);
+        Queue& operator=(const Queue&);
 
-        void enqueue(T);
-        T dequeue();
-        int size();
+        void enqueue(value_type);
+        value_type dequeue();
+        size_t size();
+
+        template<typename U>
+        friend std::ostream& operator<<(std::ostream&, Queue<U>&);
 };
