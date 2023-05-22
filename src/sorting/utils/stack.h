7fdc25605adb443bf20dbbe0cd89c9aa91b73423
bxchaney <bxchaney@gmail.com>
2023-05-21 20:29:32 -0400

    moved template definitions to header, removed smart pointers

--- src/sorting/utils/stack.h
+++ src/sorting/utils/stack.h
@@ -1,5 +1,4 @@
 #pragma once
-#include<memory>
 #include"queue.h"
 
 struct Tuple
@@ -11,19 +10,20 @@ template<typename T>
 class Stack
 {
     private:
-        typedef Node<T> node_type;
-        typedef std::shared_ptr<node_type> node_pointer;
+        using node_type = Node<T>;
+        using node_pointer = Node<T>*;
+        using value_type = T;
+        using reference = T&;
+        using const_reference = const T&;
+
 
         node_pointer _head = nullptr;
         size_t _size = 0;
 
     public:
-        typedef typename node_type::value_type value_type;
-        typedef typename node_type::reference reference;
-        typedef typename node_type::const_reference const_reference;
 
         Stack() = default;
-        ~Stack() = default;
+        ~Stack();
 
         void push(T&);
         T pop();
@@ -31,3 +31,66 @@ class Stack
         bool is_empty();
         int size();
 };
+
+template<typename T>
+Stack<T>::~Stack() 
+{
+    while(_head)
+    {
+        pop();
+    }
+}
+
+/// @brief adds a new value to the end of the stack.
+/// @tparam T type of parameter.
+/// @param value the value being added to the stack.
+template<typename T>
+void Stack<T>::push(T& value)
+{
+    node_pointer new_node = new Node(value);
+    if (!_head)
+    {
+        _head = new_node;
+    }
+    else
+    {
+        new_node->next = _head;
+        _head = new_node;
+    }
+
+    _size++;
+}
+
+/// @brief returns and removes the last value of the stack.
+/// @tparam T type of parameter.
+/// @return the value at the top of the stack. Throws an exception when the
+/// stack is empty.
+template<typename T>
+T Stack<T>::pop()
+{
+    if (!_head) throw std::exception();
+    T data = _head->data;
+    node_pointer old_node = _head;
+    _head = _head->next;
+    old_node->~Node();
+    _size--;
+    return data;
+}
+
+/// @brief Returns true when the stack is empty.
+/// @tparam T the type of values in the stack.
+/// @return 
+template<typename T>
+bool Stack<T>::is_empty()
+{
+    return _head == nullptr;
+}
+
+/// @brief Returns the number of elements in the stack.
+/// @tparam T the type of elements in the stack.
+/// @return 
+template<typename T>
+int Stack<T>::size()
+{
+    return _size;
+}
